1edaa730180c784416457cb40211c62c22ba8d05
JenniferReetz <jenniferheloisareetz@gmail.com>
2024-09-14 19:44:48 -0300

    fazendo testes

--- src/main/java/br/com/alura/service/AbrigoService.java
+++ src/main/java/br/com/alura/service/AbrigoService.java
@@ -29,14 +29,21 @@ public class AbrigoService {
         String responseBody = response.body();
         Abrigo[] abrigos = new ObjectMapper().readValue(responseBody, Abrigo[].class);
         List<Abrigo> abrigoList=Arrays.stream(abrigos).toList();
-        System.out.println("Abrigos cadastrados:");
-        for (Abrigo abrigo : abrigoList) {
-            long id = abrigo.getId();
-            String nome = abrigo.getNome();
-            System.out.println(id + " - " + nome);
+        if (abrigoList.isEmpty()){
+            System.out.println("Não há abrigos cadastrados");
+        } else{
+            mostrarAbrigos(abrigoList);
         }
     }
+private void mostrarAbrigos(List<Abrigo> abrigoList){
 
+    System.out.println("Abrigos cadastrados:");
+    for (Abrigo abrigo : abrigoList) {
+        long id = abrigo.getId();
+        String nome = abrigo.getNome();
+        System.out.println(id + " - " + nome);
+    }
+}
     public void cadastrarAbrigo() throws IOException, InterruptedException {
         System.out.println("Digite o nome do abrigo:");
         String nome = new Scanner(System.in).nextLine();
